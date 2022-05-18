3427cef48d32649a7fa91f4ea3e480a0791c5a7a
coder309533 <72287735+coder309533@users.noreply.github.com>
2022-05-17 20:12:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,117 @@
+song1 = "";
+song2 = "";
+
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
+song1_status = "";
+song2_status = "";
+
+
+
+
+function preload() {
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+
+
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX =" + rightWristX + "rightWristY = " + rightWristY);
+
+    }
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("#1100ff");
+    stroke("#1100ff");
+
+    if (scoreLeftWrist > 0.2) {
+
+        circle(leftWristX, leftWristY, 20);
+        song1.stop;
+        if (song2_status == false) {
+
+            song = song2;
+
+            song.play();
+            document.getElementById("song").innerHTML = "cartoon song ";
+            document.body.style.backgroundImage = "url('cbcg.webp')";
+        }
+    }
+
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song2.stop;
+        if (song1_status == false) {
+
+            song = song1;
+
+            song.play();
+            document.getElementById("song").innerHTML = "DJ song";
+            document.body.style.backgroundImage = "url('bcg.jpg')";
+
+        }
+
+
+
+    }
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+
+}
+
+function stop() {
+    song.stop();
+
+}
+
+function pause() {
+    song.pause();
+}
